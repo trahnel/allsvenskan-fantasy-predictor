97c58491f18a798befe3ce8dd0477b00b2f40e1e
Taavi Rahnel <taavi@spotify.com>
2024-04-02 23:38:41 +0700

    Split bootstrap static and players fetching, less load on AS servers getting players only every day

--- scripts/get_players_data.py
+++ scripts/get_players_data.py
@@ -8,11 +8,6 @@ bootstrap_static = requests.get(
     'https://fantasy.allsvenskan.se/api/bootstrap-static/')
 bootstrap_json = bootstrap_static.json()
 
-
-os.makedirs(FILES_PATH, exist_ok=True)
-with open(f'{FILES_PATH}/bootstrap-static.json', 'w+') as json_file:
-    json.dump(bootstrap_json, json_file, indent=2)
-
 players = bootstrap_json['elements']
 players_count = len(players)
 
